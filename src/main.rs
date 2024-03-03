4be0729491efc88547042925c8d8e7c1ab7ed832
FloRide1 <florian.reimat@orange.fr>
2024-03-03 18:46:30 +0100

    [MAIN] Add basic http server + GET /status endpoint
    - Add GET /status for application current status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/status", get(get_status));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Host address is not alvaible");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Axum server couldn't start");
+}
+
+async fn get_status() -> &'static str {
+    "UP"
 }

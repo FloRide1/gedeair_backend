e98e18d339ad06983e0b70316fe81274362ed6e0
FloRide1 <florian.reimat@orange.fr>
2024-03-04 11:13:42 +0100

    [TESTS] Add openapi basic tests

--- tests/misc_tests.rs
+++ tests/misc_tests.rs
@@ -1,6 +1,7 @@
 use axum::{body::Body, extract::Request, http::StatusCode};
 use gedeair_backend::app;
 use http_body_util::BodyExt;
+use serde_json::{json, Value};
 use tower::util::ServiceExt;
 
 #[tokio::test]
@@ -24,4 +25,42 @@ async fn basic_status_test() {
     let body = std::str::from_utf8(&body).unwrap();
 
     assert_eq!(body, "UP");
+}
+
+#[tokio::test]
+async fn basic_swagger_test() {
+    let app = app();
+
+    let response = app
+        .clone()
+        .oneshot(
+            Request::builder()
+                .uri("/swagger-ui")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::SEE_OTHER);
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .uri("/api-docs/openapi.json")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+
+    // Simple text for beeing sure that openapi is working, don't forget to bump version
+    assert_eq!(*body.get("openapi").unwrap(), json!("3.0.3"));
 }

81c246aaf82a093ae01842aced9383eccc07e914
FloRide1 <florian.reimat@orange.fr>
2024-03-04 14:15:35 +0100

    [MAIN] Add tracing of request in the app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use gedeair_backend::app;
+use tracing_subscriber::prelude::*;
+
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    let app = app();
+    // Launch tracing
+    let filter = tracing_subscriber::filter::Targets::new()
+        .with_target("tower_http::trace::on_response", tracing::Level::TRACE)
+        .with_target("tower_http::trace::on_request", tracing::Level::TRACE)
+        .with_target("tower_http::trace::make_span", tracing::Level::DEBUG)
+        .with_default(tracing::Level::INFO);
+
+    let tracing_layer = tracing_subscriber::fmt::layer();
+
+    tracing_subscriber::registry()
+        .with(tracing_layer)
+        .with(filter)
+        .init();
+
+    let app = app().layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await

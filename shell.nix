8d6874c049450a5dad7dd4e9ad0058c11cf4a1aa
FloRide1 <florian.reimat@orange.fr>
2024-03-04 19:17:25 +0100

    [NIX] Add sea-orm-cli into dev shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ rustc cargo rustfmt rust-analyzer clippy ];
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    clippy
+    sea-orm-cli
+  ];
 
   RUST_BACKTRACE = 1;
 }

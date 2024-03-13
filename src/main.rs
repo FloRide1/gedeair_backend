bf4e2aa6c9ffe1a8a8449cdb3080ce91c88991af
FloRide1 <florian.reimat@orange.fr>
2024-03-13 18:12:31 +0100

    [MAIN] Add database connection + auto-migration on startup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::net::SocketAddr;
+use migration::MigratorTrait;
+use std::{net::SocketAddr, time::Duration};
 
 use gedeair_backend::{app, cli::Arguments};
 use tracing_subscriber::prelude::*;
@@ -29,6 +30,27 @@ async fn main() {
             .init();
     }
 
+    let url: String;
+    if let Some(given_url) = cli.database_url {
+        url = given_url;
+    } else {
+        let username = cli.postgres_user;
+        let password = cli
+            .postgres_password
+            .expect("Password should be passed in order to connect to the database");
+        let host = cli.postgres_host;
+        let port = cli.postgres_port;
+        let database = cli.postgres_db;
+        url = format!("postgres://{username}:{password}@{host}:{port}/{database}");
+    }
+    let db = get_database_conn(&url, cli.postgres_schema)
+        .await
+        .expect("Couldn't connect to the database");
+
+    migration::Migrator::up(&db, None)
+        .await
+        .expect("Migration couldn't proceed correctly");
+
     let app = app().layer(TraceLayer::new_for_http());
 
     let address: SocketAddr = cli.address.parse().expect(
@@ -45,4 +67,23 @@ async fn main() {
     axum::serve(listener, app)
         .await
         .expect("Axum server couldn't start");
+}
+
+async fn get_database_conn(
+    url: &str,
+    default_schema: Option<String>,
+) -> Result<sea_orm::prelude::DatabaseConnection, sea_orm::prelude::DbErr> {
+    let mut opt = sea_orm::ConnectOptions::new(url);
+    opt.max_connections(50)
+        .min_connections(3)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8));
+
+    if let Some(default_schema) = default_schema {
+        opt.set_schema_search_path(default_schema);
+    }
+
+    sea_orm::Database::connect(opt).await
 }

fa4194526b891868365db4faabc39b741d64516f
FloRide1 <florian.reimat@orange.fr>
2024-03-16 22:08:00 +0100

    [CLI] Add OpenID CLI arguments

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{command, Parser};
 
 /// The backend of an RPG system
-#[derive(Parser)]
+#[derive(Parser, Debug, Default)]
 #[command(version, about, long_about = None)]
 pub struct Arguments {
     /// The address for hosting the backend in format: "IP:PORT" (IP 0.0.0.0 means broadcast, 127.0.0.1 means local)
@@ -40,4 +40,24 @@ pub struct Arguments {
     /// "postgres://<username>:<password>@<host>:<port>/<database>"
     #[arg(env, long)]
     pub database_url: Option<String>,
+
+    /// The base URL of you're Application (needed for OpenID).
+    /// Example: https://app.example.com
+    #[arg(env, long)]
+    pub application_base_url: String,
+
+    /// The OpenID Issuer URL.
+    /// Example: https://auth.example.com/realms/master
+    #[arg(env, long)]
+    pub openid_issuer: String,
+
+    /// The OpenID Client Id.
+    /// Example: my-client
+    #[arg(env, long)]
+    pub openid_client_id: String,
+
+    /// The OpenID Client Secret.
+    /// Example: 123456
+    #[arg(env, long)]
+    pub openid_client_secret: String,
 }

ff628edadcd517596c1dfffd132d9f0f24ec379d
FloRide1 <florian.reimat@orange.fr>
2024-04-01 11:55:30 +0200

    [TEST] Add basic_login_test()

---
+++ tests/base_tests.rs
@@ -0,0 +1,29 @@
+mod prepare;
+
+use crate::prepare::prepare_mock_db;
+use axum::{body::Body, extract::Request, http::StatusCode, response::IntoResponse};
+use gedeair_backend::{app, cli::Arguments};
+use tower::util::ServiceExt;
+
+#[tokio::test]
+async fn basic_login_test() {
+    let mut arguments = Arguments::default();
+    arguments.skip_oidc = true;
+
+    let db = prepare_mock_db().await;
+
+    let app = app(db, arguments).await;
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .uri("/login")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::SEE_OTHER);
+}

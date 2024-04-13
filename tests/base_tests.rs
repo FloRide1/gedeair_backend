489bb5ed5548d6151774de132fcd03aa0ee6d259
FloRide1 <florian.reimat@orange.fr>
2024-04-13 14:46:23 +0200

    feat(test): Add basic_me_test() test

--- tests/base_tests.rs
+++ tests/base_tests.rs
@@ -1,8 +1,10 @@
 mod prepare;
 
 use crate::prepare::prepare_mock_db;
-use axum::{body::Body, extract::Request, http::StatusCode, response::IntoResponse};
+use axum::{body::Body, extract::Request, http::StatusCode};
 use gedeair_backend::{app, Arguments};
+use http_body_util::BodyExt;
+use serde_json::{json, Value};
 use tower::util::ServiceExt;
 
 #[tokio::test]
@@ -26,4 +28,35 @@ async fn basic_login_test() {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::SEE_OTHER);
+}
+
+#[tokio::test]
+async fn basic_me_test() {
+    let mut arguments = Arguments::default();
+    arguments.skip_oidc = true;
+
+    let db = prepare_mock_db().await;
+
+    let app = app(db, arguments).await;
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .uri("/user")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+
+    assert_eq!(*body.get("id").unwrap(), json!("1"));
+    assert_eq!(*body.get("username").unwrap(), json!("test_username"));
+    assert_eq!(*body.get("name").unwrap(), json!("test name"));
+    assert_eq!(*body.get("email").unwrap(), json!("test@test.com"));
 }
